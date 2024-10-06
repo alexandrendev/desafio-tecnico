5e1516795edee3bdf24d834c7e7a7015b019eead
alexandrendev <alxfn86@gmail.com>
2024-10-06 14:32:35 -0300

    add: itens 3 e 4 do desafio

---
+++ desafio-tecnico/src/main/java/com/alexandre/item3/Soma.java
@@ -0,0 +1,14 @@
+package com.alexandre.item3;
+
+public class Soma {
+    public static void main(String[] args) {
+        int i = 11;
+        int k = 1;
+        int soma = 0;
+        while (k<i){
+            k = k+1;
+            soma += k;
+        }
+        System.out.println(soma);
+    }
+}
